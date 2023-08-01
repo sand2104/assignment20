070817a071e8b833424fe0f3616b7aee2698e29e
sand2104 <prasadsandy2000@gmail.com>
2023-08-01 15:44:27 +0530

    first commit

---
+++ Library.cs
@@ -0,0 +1,70 @@
+﻿using aasignment20;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment20
+{
+    public class Library
+    {
+        static List<Book> books;
+        public Library()
+        {
+            books = new List<Book>()
+            {
+                new Book() {BookId=1,Title="Java",Author="Andrew Stellman",Genre="2007",IsAvailable=true},
+                new Book() {BookId=2,Title="C#",Author="Stephen Cleary",Genre="2014",IsAvailable=true},
+                new Book() {BookId=3,Title="Python",Author="Matthew MacDonald",Genre="2005",IsAvailable=true},
+                new Book() {BookId=4,Title="Mysql",Author="Ian Griffiths",Genre="2012",IsAvailable=true},
+                new Book() {BookId=5,Title="Angular",Author="Andrew Troelsen",Genre="2015",IsAvailable=true}
+            };
+        }
+        public void AddBook(Book b)
+        {
+            books.Add(b);
+            Console.WriteLine("List of books");
+            this.ViewAllBook();
+        }
+        public void ViewAllBook()
+        {
+            foreach (Book book in books)
+            {
+                Console.WriteLine("Book Id: " + book.BookId);
+                Console.WriteLine("Title: " + book.Title);
+                Console.WriteLine("Author: " + book.Author);
+                Console.WriteLine("Genre: " + book.Genre);
+                Console.WriteLine("IsAvailable: " + book.IsAvailable);
+            }
+        }
+        public void SearchBookById(int id)
+        {
+            foreach (Book book in books)
+            {
+                if (book.BookId == id)
+                {
+                    Console.WriteLine("Book Id: " + book.BookId);
+                    Console.WriteLine("Title: " + book.Title);
+                    Console.WriteLine("Author: " + book.Author);
+                    Console.WriteLine("Genre: " + book.Genre);
+                    Console.WriteLine("IsAvailable: " + book.IsAvailable);
+                }
+            }
+        }
+        public void SearchBookByTitle(string title)
+        {
+            foreach (Book book in books)
+            {
+                if (book.Title == title)
+                {
+                    Console.WriteLine("Book Id: " + book.BookId);
+                    Console.WriteLine("Title: " + book.Title);
+                    Console.WriteLine("Author: " + book.Author);
+                    Console.WriteLine("Genre: " + book.Genre);
+                    Console.WriteLine("IsAvailable: " + book.IsAvailable);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using aasignment20;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ch;
+            do
+            {
+                Console.WriteLine("1.Add a Book \n2.View all Books \n3.Search by Id \n4.Search by Title \n5.Exit the program");
+                int op = int.Parse(Console.ReadLine());
+                Library library = new Library();
+                switch (op)
+                {
+                    case 1:
+                        {
+                            Book book = new Book();
+                            Console.WriteLine("Enter Book Id: ");
+                            book.BookId = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter Title: ");
+                            book.Title = Console.ReadLine();
+                            Console.WriteLine("Enter Author: ");
+                            book.Author = Console.ReadLine();
+                            Console.WriteLine("Enter Genre: ");
+                            book.Genre = Console.ReadLine();
+                            Console.WriteLine("Enter is Available: ");
+                            book.IsAvailable = bool.Parse(Console.ReadLine());
+                            library.AddBook(book);
+                            break;
+                        }
+                    case 2:
+                        {
+                            library.ViewAllBook();
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("ENter id to search: ");
+                            int id = int.Parse(Console.ReadLine());
+                            library.SearchBookById(id);
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.WriteLine("Enter title to search: ");
+                            string title = Console.ReadLine();
+                            library.SearchBookByTitle(title);
+                            break;
+                        }
+                    case 5:
+                        {
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Invalid Choice!!");
+                            break;
+                        }
+                }
+                Console.WriteLine("Do you Want to Continue? ");
+                ch = Console.ReadLine().ToLower();
+            } while (ch == "y");
+            Console.ReadKey();
+        }
+    }
+}
